6b325bd864e7eca154824ea5edb7c966cb309e3b
nathan <seunggyu0128@gmail.com>
2020-05-08 11:23:38 +1200

    half way done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,5 @@
-import '../styles/styles.css' //loading css file in the main javascript file, webpack only works with javascript so must install
+import '../styles/styles.css' //loading css file in the main javascript file, webpack only works with javascript so must install css-loader style-loader
 
-if (module.hot) {
-	module.hot.accept()
+if (module.hot) {  //accept updates on the fly
+	module.hot.accept() //accept the hot updates if it makes sense to accept them, When this module or dependencies are updated, this module can be disposed and re-evaluated without informing parents. This makes sense if this module has no exports (or exports are updated in another way). 
 }
